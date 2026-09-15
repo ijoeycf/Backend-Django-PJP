5fab51b615dd4da0fa5e9f0fb2fcd5c8890eeeb0
ijoeycf <joanadogas1@gmail.com>
2026-09-14 21:52:56 -0300

    Atividade

--- appJoana/views.py
+++ appJoana/views.py
@@ -17,3 +17,29 @@ def sobre(request):
 
     if request.method == "GET":
         return render (request, "index.html", dicionario)
+
+
+def atividade(request):
+    dic = {
+        "numero": None,
+        "tabuada":[]
+    }
+
+    if request.method == "POST":
+
+        dic ["numero"] = request.POST.get("InputNumero")
+        numero = request.POST.get("InputNumero")
+
+        if numero:
+             numero = int(numero)
+
+        for i in range(0,11):
+                valor = numero * i
+                dic["tabuada"].append(valor)
+                i+=1
+
+        return render(request, "atividade.html", dic)
+
+    if request.method == "GET":
+        return render(request, "atividade.html", dic)
+
--- projetoJoana/urls.py
+++ projetoJoana/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from appJoana.views import sobre
+from appJoana.views import sobre, atividade
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('sobre/', sobre)
+    path('sobre/', sobre),
+    path('atividade/', atividade)
 ]
